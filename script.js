f9577a1ee784e787c9bd8616e640b84180fe9950
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-08-15 21:50:00 -0300

    Sanderson-BarbosaC-ex-a015

---
+++ script.js
@@ -0,0 +1,57 @@
+//ELEMENTOS NECESSÁRIOS
+/* Inicialização de váriável*/
+/* Condição de continuação*/
+/* Um conjunto de ações*/
+/* Uma alteração*/
+
+/* let numero = Number(prompt("Digite um número para somar e um 0 para sair"))
+
+let soma = 0
+
+while(numero !== 0){
+    soma = soma+numero
+    console.log(soma);
+    console.log(numero);
+    numero = Number(prompt("Digite um número para somar e um 0 para sair"))
+}
+
+console.log(soma);
+console.log("X" .repeat(50)); */
+
+/* const numero2 = +prompt("Digite um número")
+
+for (let i = 0; i<= numero2; i++){
+    console.log(i);
+} */
+
+/* const listaDeNumero = [2,4,6,8,10,12,14]
+
+for(let i = 0; i < listaDeNumero.length; i++){
+    console.log(`O número do idice ${i} é ${listaDeNumero[i]}`);
+}
+
+console.log("Fim"); */
+
+/* let tipoDeUsuario = prompt(`Qual o seu nível de Usuário?
+A - Administrador
+B - Usuário Comum
+C - Assinante`).toUpperCase()
+
+console.log(tipoDeUsuario);
+
+console.log("X".repeat(50));
+
+while(tipoDeUsuario !== "A"){
+    alert("Acesso Negado!");
+    tipoDeUsuario = prompt(`Qual o seu nível de Usuário?
+    A - Administrador
+    B - Usuário Comum
+    C - Assinante`).toUpperCase()
+}
+alert("Acesso liberado") */
+
+
+
+for(let i = 1; i <= 10; i++ ){
+    console.log(i*2);
+}
